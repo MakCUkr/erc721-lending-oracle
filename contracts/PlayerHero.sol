cca559b4f058c2c4c3fcb7e71d26d6963ca579db
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-06-14 17:52:35 +0100

    added erc721

---
+++ contracts/PlayerHero.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+
+/*
+    ERC721 implementation : https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol
+    The ERC721 contract inherits from the following interfaces: IERC721Metadata
+    The ERC721 contract inherits from the following contracts: Context, ERC165, IERC721
+*/
+contract PlayerHero is ERC721,  Ownable{
+    using Strings for uint256;
+
+    string private _stringBaseURI = "";
+    uint public mintCost = 0.1 ether;
+    constructor(string memory _name, string memory _symbol, string memory _newStringBaseURI)
+    ERC721(_name, _symbol)
+    {
+        _stringBaseURI = _newStringBaseURI;
+    }
+
+    function setBaseUri(string memory _newBaseUri) public onlyOwner {
+        _stringBaseURI = _newBaseUri;
+    }
+
+
+    function _baseURI() internal view virtual override(ERC721) returns (string memory) {
+        return _stringBaseURI;
+    }
+
+    function mint(address to, uint256 tokenId) public payable {
+        require(msg.value == mintCost, string(abi.encodePacked("mintCost not paid. mintCost: ", mintCost.toString(), " wei")));
+        _mint(to, tokenId);
+    }
+}
---
+++ test/sample-test.js
@@ -0,0 +1,45 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const nameErc721 = "Thetan Hero";
+const symbolErc721 = "THTN";
+const uriErc721 = "https://antonymnft.s3.us-west-1.amazonaws.com/json/";
+
+describe("Greeter", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const Greeter = await ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, world!");
+    await greeter.deployed();
+
+    expect(await greeter.greet()).to.equal("Hello, world!");
+
+    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+    // wait until the transaction is mined
+    await setGreetingTx.wait();
+
+    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  });
+});
+
+
+describe("Player Hero", function () {
+  it("Should mint an NFT and assign base Uri", async function () {
+    const [owner, addr1] = await ethers.getSigners();
+
+    const PlayerHero = await ethers.getContractFactory("PlayerHero");
+    const playerHero = await PlayerHero.deploy(nameErc721, symbolErc721, "");
+    await playerHero.deployed();
+
+    expect(await playerHero.owner()).to.equal(owner.address);
+
+    const setBaseUriTxn = await playerHero.setBaseUri(uriErc721);
+    // wait until the transaction is mined
+    await setBaseUriTxn.wait();
+    const mintTokenIdTxn = await playerHero.connect(addr1).mint(addr1.address, 1, {value: ethers.utils.parseEther("0.1")})
+    expect(await playerHero.tokenURI(1)).to.equal(uriErc721+"1");
+  });
+
+
+  
+});
